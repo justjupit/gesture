d3784a3d6b2452ce2d6cf1412eaaad89a44b9667
justjupit <justjupit@gmail.com>
2021-04-12 15:24:09 +0800

    final 04/12

--- app/src/main/java/com/jupiter/application/gesture/MainActivity.kt
+++ app/src/main/java/com/jupiter/application/gesture/MainActivity.kt
@@ -10,66 +10,114 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, View.OnTouchListener {
 
     lateinit var gDetector: GestureDetector
+    var flag:Boolean = true
+    var pictureNo:Int = 0
+    var totalPictures:Int = 7
+
+
+    fun showPicture(){
+        /*
+        when (PictureNo){
+            0 -> img.setImageResource(R.drawable.pu0)
+            1 -> img.setImageResource(R.drawable.pu1)
+            2 -> img.setImageResource(R.drawable.pu2)
+            3 -> img.setImageResource(R.drawable.pu3)
+        }
+        */
+
+        var res:Int = getResources().getIdentifier("pu" + (pictureNo),
+            "drawable", getPackageName())
+        img.setImageResource(res)
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gDetector = GestureDetector(this,this)
         img.setOnTouchListener(this)
+        var res:Int = -1
+        var countDrawables:Int = -1
+        while (res != 0) {
+            countDrawables++;
+            res = getResources().getIdentifier("pu" + (countDrawables),
+                "drawable", getPackageName());
+        }
+        totalPictures = countDrawables
+        txv.text=pictureNo.toString()
     }
-
+/*
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        gDetector.onTouchEvent(event)
+        return true
+    }
+*/
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
         gDetector.onTouchEvent(event)
         return true
     }
 
     override fun onDown(p0: MotionEvent?): Boolean {
-        txv.text = "按下"
+        flag=true
         return true
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-        txv.text = "短按"
+        pictureNo = 0
+        showPicture()
+        txv.text=pictureNo.toString()
         return true
     }
 
     override fun onLongPress(p0: MotionEvent?) {
-        txv.text = "長按"
+
     }
 
 
     override fun onShowPress(p0: MotionEvent?) {
-        txv.text = "按下後無後續動作"
+
     }
 
     override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
-        txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
-                "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString()
-        return true
-    }
+        if(flag){
+            if (e1.getY() < e2.getY()){
+            pictureNo++
+             if (pictureNo == totalPictures) {
+                  pictureNo = 0
+            }
+        } else {
+            pictureNo--;
+             if (pictureNo < 0) {
+                 pictureNo = totalPictures - 1
+            }
+        }
+         showPicture()
+         txv.text = pictureNo.toString()
+        }
+            flag = false
+            return true
+        }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
-                "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString() +
-                "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+
         return true
     }
 
     override fun onDoubleTap(p0: MotionEvent?): Boolean {
+        pictureNo = totalPictures - 1
+        showPicture()
+        txv.text=pictureNo.toString()
         return true
     }
 
     override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
-        txv.text = "連續點兩下"
+
         return true
     }
 
     override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
         return true
-    }
-
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        TODO("Not yet implemented")
     }
 
 }
